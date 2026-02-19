6c8a8d5e49505f97601379164388169296f0ef49
SnR-Test <snrtest9@gmail.com>
2026-02-19 09:56:28 +0600

    fix: resolve detected issues

--- common/middlewares/CheckPermissionMiddleware.js
+++ common/middlewares/CheckPermissionMiddleware.js
@@ -1,35 +1,31 @@
-const UserModel = require("../models/User");
+UserModel.findUser({ id: userId })
+        .then((user) => {
+          // IF user does not exist in our database, means something is fishy
+          // THEN we will return forbidden error and ask user to login again
+          if (!user) {
+            return res.status(403).json({
+              status: false,
+              error: "Invalid access token provided, please login again.",
+            });
+          }
 
-module.exports = {
-  has: (role) => {
-    return (req, res, next) => {
-      const {
-        user: { userId },
-      } = req;
+          const userRole = user.role;
 
-      UserModel.findUser({ id: userId }).then((user) => {
-        // IF user does not exist in our database, means something is fishy
-        // THEN we will return forbidden error and ask user to login again
-        if (!user) {
-          return res.status(403).json({
+          // IF user does not possess the required role
+          // THEN return forbidden error
+          if (userRole !== role) {
+            return res.status(403).json({
+              status: false,
+              error: `You need to be a ${role} to access this endpoint.`,
+            });
+          }
+
+          next();
+        })
+        .catch((error) => {
+          console.error('Error checking user permissions:', error);
+          return res.status(500).json({
             status: false,
-            error: "Invalid access token provided, please login again.",
+            error: "Internal server error occurred while checking permissions.",
           });
-        }
-
-        const userRole = user.role;
-
-        // IF user does not possess the required role
-        // THEN return forbidden error
-        if (userRole !== role) {
-          return res.status(403).json({
-            status: false,
-            error: `You need to be a ${role} to access this endpoint.`,
-          });
-        }
-
-        next();
-      });
-    };
-  },
-};
+        });
